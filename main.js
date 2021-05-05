8d6d262572718e2be5bac2fb5e869b8604fba1a5
CoderPulkit <75306428+CoderPulkit@users.noreply.github.com>
2021-05-05 19:38:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+var menulistarray=["Veg-vegetable Pizza",
+"Veg-Supreme Pizza", "Paneer-Tikka Pizza", "Margherita Pizza", "Veg extravaganza pizza", 
+"Pasta Pizza" ];
+
+function getmenu() {
+    var htmldata;
+htmldata="<ol class='menulist'>";
+menulistarray.sort();
+for (var i=0;i<menulistarray.length; i++)
+{
+    htmldata=htmldata+'<li>'+menulistarray[i]+'</li>';
+}
+htmldata=htmldata+"</ol>";
+document.getElementById("display_menu").innerHTML=htmldata;
+}
+function add_item() {
+    var htmldata;
+    var item=document.getElementById("add_item").value;
+    menulistarray.push(item);
+    menulistarray.sort();
+    htmldata="<section class='cards'>";
+    for (var i=0;i<menulistarray.length;i++){
+        htmldata=htmldata+'<div class="card">'+'<img src="images/pizzaImg.png"/>'+menulistarray[i]+'</div>';
+        }
+        htmldata=htmldata+"</section>";
+        document.getElementById("display_addedmenu").innerHTML=htmldata;
+        
+}
